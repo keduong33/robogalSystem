c139ae8481840d6992a4e7c1fb3bfd1e20a8e798
Ke Duong <kelyduong@gmail.com>
2022-12-09 14:40:45 +1100

    Initial Login Page Commit

---
+++ pages/Login.tsx
@@ -0,0 +1,106 @@
+import Image from "next/image";
+import React from "react";
+import LogoBar from "./components/LogoBar";
+
+type Props = {};
+
+function Login({}: Props) {
+  return (
+    <main className="bg-gradient-to-br from-blue-50 via-blue-400 to-blue-200 min-h-screen">
+      {/* login container */}
+      <div className="h-full flex flex-col">
+        <div className="">
+          <LogoBar />
+        </div>
+        <div className="h-full flex items-center justify-center">
+          {/* Login container */}
+          <div className="border rounded-3xl md:w-6/12 sm:w-2/3 lg:w-1/3 bg-opacity-30 bg-slate-200 p-6 sm:p-10 md:p-20 lg:p-10">
+            {/* Form container */}
+            <form>
+              <div className="mb-8">
+                <h1 className="block text-6xl font-bold mb-4 text-black">
+                  Sign in
+                </h1>
+                <h2 className="text-black font-bold text-md">
+                  Book a Robogals session for your school
+                </h2>
+              </div>
+              <div className="mb-4">
+                <label className="block text-grey-darker text-sm font-bold mb-2">
+                  Email
+                </label>
+                <input
+                  className="shadow appearance-none border border-black rounded w-full py-2 px-3 text-black bg-transparent placeholder-black"
+                  id="email"
+                  type="text"
+                  placeholder="Enter your email"
+                />
+              </div>
+              <div className="">
+                <label className="block text-grey-darker text-sm font-bold mb-2">
+                  Password
+                </label>
+                <input
+                  className="shadow appearance-none border border-black rounded w-full py-2 px-3 text-grey-darker mb-3 bg-transparent
+                  placeholder-black"
+                  id="password"
+                  type="password"
+                  placeholder="Enter your Password Password"
+                />
+              </div>
+              <div className="flex flex-col justify-between">
+                <a
+                  className="inline-block align-baseline font-bold text-sm text-black underline text-right mb-6"
+                  href="#"
+                >
+                  Forgot Password?
+                </a>
+                <button
+                  className="bg-color-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg w-full"
+                  type="submit"
+                >
+                  Sign In
+                </button>
+              </div>
+            </form>
+            {/* Sign in with Google Button */}
+            <div className="mt-2 flex">
+              <button
+                type="button"
+                className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex justify-center items-center dark:focus:ring-[#4285F4]/55 mb-2 w-full"
+              >
+                <svg
+                  className="mr-2 -ml-1 w-4 h-4"
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fab"
+                  data-icon="google"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 488 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+                  ></path>
+                </svg>
+                Sign in with Google
+              </button>
+            </div>
+            {/* Sign up */}
+            <div className="mt-8 w-full text-center">
+              <label>
+                Don&apos;t have an account?{" "}
+                <a className="font-bold underline" href="#">
+                  Register
+                </a>
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default Login;
---
+++ pages/components/LogoBar.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import React from "react";
+
+type Props = {};
+
+function LogoBar({}: Props) {
+  return (
+    <div className="w-full">
+      <div className="flex justify-center">
+        <Image
+          src="/logo.png"
+          alt="Logo"
+          width={200}
+          height={200}
+          className="w-4/12 md:w-3/12 lg:w-1/6 "
+        />
+      </div>
+    </div>
+  );
+}
+
+export default LogoBar;
