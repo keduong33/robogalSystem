78637ac931edfdf1542b20d250e2c147a21a0f16
Ke Duong <kelyduong@gmail.com>
2023-04-01 00:36:46 +1100

    Update the design of Bookings List

--- components/BookingComps/BookingListComp.jsx
+++ components/BookingComps/BookingListComp.jsx
@@ -10,6 +10,7 @@ import {
   TableHead,
   TableRow,
 } from "@mui/material";
+import { BorderBottom } from "@mui/icons-material";
 
 function BookingListComp() {
   const { user } = useAuth();
@@ -27,9 +28,30 @@ function BookingListComp() {
 
   const sessionList = [
     createData("Intro 1", "Confirmed", "1/1/2001", "1AM-2AM", "Your primary"),
-    createData("Intro 2", "Unconfirmed", "2/2/2002", "1AM-2AM", "Location"),
+    createData("Intro 2", "Pending", "2/2/2002", "1AM-2AM", "Location"),
     createData("Lego", "Declined", "3/3/2003", "1AM-2AM", "Location"),
   ];
+
+  const getStatusColor = (status) => {
+    switch (status) {
+      case "Confirmed":
+        return "#E3F2FE"; //blue
+      case "Pending":
+        return "#DDDDDD"; //grey
+      case "Declined":
+        return "#EBCCCC"; //pink
+      default:
+        return "transparent";
+    }
+  };
+
+  const NormalTableCell = ({ value }) => {
+    return (
+      <TableCell sx={{ borderBottom: "transparent" }} align="right">
+        {value}
+      </TableCell>
+    );
+  };
 
   return (
     // <div>
@@ -38,27 +60,49 @@ function BookingListComp() {
     //   ))} */}
     //   <Table></Table>
     // </div>
-    <TableContainer className="min-w-fit max-w-screen-xl">
-      <Table aria-label="simple table">
-        <TableHead>
-          <TableRow sx={{ border: 0 }}>
-            <TableCell>Session Name</TableCell>
-            <TableCell align="right">Status</TableCell>
-            <TableCell align="right">Date&nbsp;</TableCell>
-            <TableCell align="right">Time&nbsp;</TableCell>
-            <TableCell align="right">Location&nbsp;</TableCell>
+    <TableContainer className="min-w-fit max-w-screen-xl max-h-80">
+      <Table aria-label="Booking list" className="h-fit">
+        <TableHead stickyHeader>
+          <TableRow>
+            <TableCell sx={{ borderBottom: "transparent" }}>
+              Session Name
+            </TableCell>
+            <NormalTableCell value={"Status"} />
+            <NormalTableCell value={"Date"} />
+            <NormalTableCell value={"Time"} />
+            <NormalTableCell value={"Location"} />
           </TableRow>
         </TableHead>
         <TableBody>
           {sessionList.map((sessionObj) => (
-            <TableRow key={sessionObj.title} sx={{ border: 0 }}>
-              <TableCell component="th" scope="row">
+            <TableRow
+              key={sessionObj.title}
+              sx={{
+                backgroundColor: getStatusColor(sessionObj.status),
+              }}
+            >
+              <TableCell
+                sx={{
+                  borderBottom: "transparent",
+                  borderTopLeftRadius: 10,
+                  borderBottomLeftRadius: 10,
+                }}
+              >
                 {sessionObj.title}
               </TableCell>
-              <TableCell align="right">{sessionObj.status}</TableCell>
-              <TableCell align="right">{sessionObj.date}</TableCell>
-              <TableCell align="right">{sessionObj.time}</TableCell>
-              <TableCell align="right">{sessionObj.location}</TableCell>
+              <NormalTableCell value={sessionObj.status} />
+              <NormalTableCell value={sessionObj.date} />
+              <NormalTableCell value={sessionObj.time} />
+              <TableCell
+                sx={{
+                  borderBottom: "transparent",
+                  borderTopRightRadius: 10,
+                  borderBottomRightRadius: 10,
+                }}
+                align="right"
+              >
+                {sessionObj.location}
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
