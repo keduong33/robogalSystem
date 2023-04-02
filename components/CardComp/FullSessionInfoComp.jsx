52cb8bf300762f092e1fe862b0df074b0b845fb5
Ke Duong <kelyduong@gmail.com>
2023-04-03 00:58:08 +1000

    fix the full session info list

--- components/CardComp/FullSessionInfoComp.jsx
+++ components/CardComp/FullSessionInfoComp.jsx
@@ -11,17 +11,22 @@ function FullSessionInfoComp({ moreInfo, setMoreInfo, info }) {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 bg-slate-100 border-2 border-black border-solid shadow-slate-50 p-4 w-full lg:w-[60em] max-h-80 overflow-hidden overflow-y-scroll ">
-          <Typography id="modal-modal-title" variant="h6" component="h2">
+        <Box className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 bg-slate-100 border-2 border-black border-solid shadow-slate-50 p-4 w-full lg:w-[60em] xl:w-[70em] max-h-80  overflow-y-auto text-black flex-col">
+          <Typography
+            id="modal-modal-title"
+            variant="h6"
+            component="h2"
+            sx={{ alignSelf: "start" }}
+          >
             {info.title}
           </Typography>
           <Typography
             id="modal-modal-description"
-            sx={{ mt: 2, textAlign: "justify" }}
+            sx={{ mt: 2, textAlign: "justify", alignSelf: "center" }}
           >
             {info.longDescription}
           </Typography>
-          <button className="greenButton mt-4 w-fit">
+          <button className="greenButton w-fit mt-4">
             Select This Session
           </button>
         </Box>
--- components/CardComp/SessionListTemplateComp.jsx
+++ components/CardComp/SessionListTemplateComp.jsx
@@ -35,10 +35,20 @@ function SessionTemplateListComp() {
     date,
     time,
     location,
+    shortDescription,
     longDescription,
     imageUrl
   ) {
-    return { title, status, date, time, location, longDescription, imageUrl };
+    return {
+      title,
+      status,
+      date,
+      time,
+      location,
+      shortDescription,
+      longDescription,
+      imageUrl,
+    };
   }
 
   const templateList = [
@@ -48,6 +58,7 @@ function SessionTemplateListComp() {
       "1/1/2001",
       "1AM-2AM",
       "Your primary",
+      "Short",
       "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32.",
       "/images/test.jpg"
     ),
@@ -57,6 +68,7 @@ function SessionTemplateListComp() {
       "2/2/2002",
       "1AM-2AM",
       "Location",
+      "Short",
       "This is a long desc",
       "/images/test.jpg"
     ),
@@ -66,6 +78,7 @@ function SessionTemplateListComp() {
       "3/3/2003",
       "1AM-2AM",
       "Location",
+      "Short",
       "This is a long desc",
       "/images/test.jpg"
     ),
